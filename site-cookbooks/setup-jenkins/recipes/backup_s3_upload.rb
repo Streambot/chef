a538286b0218dbf251c892a80015e5c4024e20e0
Martin Biermann <info@martinbiermann.com>
2013-12-15 16:31:46 +0100

    Fixes Jenkins backup S3 upload cron

--- site-cookbooks/setup-jenkins/recipes/backup_s3_upload.rb
+++ site-cookbooks/setup-jenkins/recipes/backup_s3_upload.rb
@@ -2,19 +2,19 @@ script_location = "/opt/upload_jenkins_backup.sh"
 
 # Install the backup upload script from template
 template script_location do
-	source "upload_jenkins_backup.sh.erb"
-	mode 0755
-	owner "ubuntu"
-	group "ubuntu"
-	variables({
-		:s3_backup_bucket_uri => node[:aws][:s3][:backup_bucket_uri]
-	})
+  source "upload_jenkins_backup.sh.erb"
+  mode 0755
+  owner "ubuntu"
+  group "ubuntu"
+  variables({
+    :s3_backup_bucket_uri => node[:aws][:s3][:backup_bucket_uri]
+  })
 end
 
 # Register the upload script in crontab to have it executed once every hour (at minute '30 to 
 # prevent collision with actual backup from Jenkins)
-cron "cassyd" do
-  command ".#{script_location}"
+cron "jenkins_backup_s3_upload" do
+  command script_location
   minute "30"
   hour "*"
   day "*"
