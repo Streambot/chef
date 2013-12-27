ee4b2f2b9faf730498ab5d09e20ac201f9fc43b7
Martin Biermann <info@martinbiermann.com>
2013-12-27 12:01:08 +0100

    Improves golang profile loading for interactive shells in API default recipe

--- cookbooks/chef-streambot-api/recipes/default.rb
+++ cookbooks/chef-streambot-api/recipes/default.rb
@@ -2,17 +2,10 @@ include_recipe "golang"
 include_recipe "chef-golang"
 include_recipe "golang::packages"
 
-bash "load_etc_profile_on_interactive_shells" do
+bash "load_golang_profile_on_interactive_shells" do
 	user	:root
 	cwd		"/tmp"
 	code 	<<-EOH
-	echo "if [ -d /etc/profile.d ]; then
-  for i in /etc/profile.d/*.sh; do
-    if [ -r $i ]; then
-      . $i
-    fi
-  done
-  unset i
-fi" >> /etc/bash.bashrc
+	echo '[[ -s "/etc/profile.d/golang.sh" ]] && . "/etc/profile.d/golang.sh"' >> /etc/bash.bashrc
 	EOH
 end
