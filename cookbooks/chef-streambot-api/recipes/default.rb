509e0f36e1b5b1d2d7fb5ecff4fbc1f865d5ffb3
Martin Biermann <info@martinbiermann.com>
2013-12-27 11:50:31 +0100

    Adds loading of /etc/profile for interactive shells within /etc/bash.bashrc

--- cookbooks/chef-streambot-api/recipes/default.rb
+++ cookbooks/chef-streambot-api/recipes/default.rb
@@ -1,3 +1,11 @@
 include_recipe "golang"
 include_recipe "chef-golang"
 include_recipe "golang::packages"
+
+bash "load_etc_profile_on_interactive_shells" do
+	user	:root
+	cwd		"/tmp"
+	code 	<<-EOH
+	echo ". /etc/profile" >> /etc/bash.bashrc
+	EOH
+end
