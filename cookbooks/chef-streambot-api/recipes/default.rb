d744c77c900112d05ebb5516ba39496024e5d96f
Martin Biermann <info@martinbiermann.com>
2013-12-27 11:56:08 +0100

    Fixes profile loading in bash.bashrc

--- cookbooks/chef-streambot-api/recipes/default.rb
+++ cookbooks/chef-streambot-api/recipes/default.rb
@@ -6,6 +6,13 @@ bash "load_etc_profile_on_interactive_shells" do
 	user	:root
 	cwd		"/tmp"
 	code 	<<-EOH
-	echo ". /etc/profile" >> /etc/bash.bashrc
+	echo "if [ -d /etc/profile.d ]; then
+  for i in /etc/profile.d/*.sh; do
+    if [ -r $i ]; then
+      . $i
+    fi
+  done
+  unset i
+fi" >> /etc/bash.bashrc
 	EOH
 end
