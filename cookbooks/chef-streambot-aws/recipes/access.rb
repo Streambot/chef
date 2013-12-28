3c3fe15481024021f755200665b8571bd6ef3cb6
Martin Biermann <info@martinbiermann.com>
2013-12-28 22:56:39 +0100

    Fixes user reference in deploying SSH authorized keys with aws cookbook's access recipe

--- cookbooks/chef-streambot-aws/recipes/access.rb
+++ cookbooks/chef-streambot-aws/recipes/access.rb
@@ -1,3 +1,6 @@
-node.access.ssh.authorized_keys.each do |key| 
-	bash "echo '#{key}' >> ~/.ssh/authorized_keys"
+node.access.ssh.authorized_keys.each do |key|
+        bash "add_ssh_authorized_key" do
+                user node.access.ssh.user
+                code "echo '#{key}' >> ~/.ssh/authorized_keys"
+        end
 end
