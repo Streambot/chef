c78a0a70a9caf3c266b7fbd8deeceed5c44d1244
Martin Biermann <info@martinbiermann.com>
2013-12-29 22:06:56 +0100

    Fixes crashing AWS CLI recipe

--- cookbooks/chef-streambot-aws/recipes/cli.rb
+++ cookbooks/chef-streambot-aws/recipes/cli.rb
@@ -5,7 +5,7 @@ template aws_cli_config_file do
 	mode 	00600
     owner 	user[:name]
     group 	user[:group]
-    source 	'aws_cli_config.erb'
+    source 	'cli_config.erb'
     not_if 	{ ::File.exists?(aws_cli_config_file) }
     variables(user)
 end
