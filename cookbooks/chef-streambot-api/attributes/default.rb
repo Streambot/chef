6ce315205a67a9c9fe4eaf8c77c531ab58544347
Martin Biermann <info@martinbiermann.com>
2014-01-24 11:35:44 +0100

    Updates golang package dependencies

--- cookbooks/chef-streambot-api/attributes/default.rb
+++ cookbooks/chef-streambot-api/attributes/default.rb
@@ -4,11 +4,9 @@ normal["go"] = {
 	:binary => "/usr/local/go/bin/go",
 	:gopath => "/opt/go",
 	:packages => [
-		"github.com/hoisie/web",
 		"code.google.com/p/go-uuid/uuid",
 		"github.com/sqs/go-rexster-client",
-		"code.google.com/p/gorest",
-		"code.google.com/p/gomock",
-		"github.com/op/go-logging"
+		"github.com/op/go-logging",
+		"github.com/laurent22/ripple"
 	]
 }
