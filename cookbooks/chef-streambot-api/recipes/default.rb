453b625dc958da93b5d5cf5cea1849ec3cdcb03b
Martin Biermann <info@martinbiermann.com>
2014-01-24 13:01:33 +0100

    Integrates StatsD recipe into API default recipe

--- cookbooks/chef-streambot-api/recipes/default.rb
+++ cookbooks/chef-streambot-api/recipes/default.rb
@@ -1,3 +1,9 @@
+include_recipe "statsd"
 include_recipe "golang"
 include_recipe "chef-golang"
 include_recipe "golang::packages"
+
+# Verify, that StatsD is running on the machine
+service "statsd" do
+	action :restart
+end
