bd637b8c6e4972c8d73ad175a575b4bea1cf9904
Martin Biermann <info@martinbiermann.com>
2014-01-26 11:58:10 +0100

    Refactors AWS instance recipe to install tags updater script and run it on time of provisioning and on every reboot

--- cookbooks/chef-streambot-aws/recipes/instance.rb
+++ cookbooks/chef-streambot-aws/recipes/instance.rb
@@ -2,29 +2,46 @@ include_recipe "chef-streambot-aws::access"
 include_recipe "chef-streambot-aws::cli"
 
 ################################################################################
-# Update AWS name tag for Streambot API server machine
+# Install script for updating AWS tags for Streambot API server machine
 ################################################################################
 
-instance = node[:aws_instance]
+script 		= "/etc/update-aws-tags.sh"
+instance 	= node[:aws_instance]
 
-bash "update-aws-instance-name-tag" do
+template script do
+	source 	"update-aws-tags.sh.erb"
+	mode 	0755
+	owner 	"root"
+	group 	"root"
+	variables({
+		:env	=> instance[:env],
+		:role 	=> instance[:role]
+	})
+end
+
+################################################################################
+# Execute script for updating AWS tags for Streambot API server machine
+################################################################################
+
+bash "update_aws_tags" do
+	cwd File.dirname(script)
+	user "root"
+	group "root"
 	code <<-EOH
-	aws ec2 create-tags \
-	--resources `ec2metadata --instance-id` \
-	--tags Key=Name,Value=\
-	#{instance[:role]}_#{instande[:env]}_`ec2metadata availability-zone`_\
-	`ec2metadata --local-ipv4 | tr "." "-"`
-	EOH
-	user node[:aws_cli][:user]
+	 ./#{File.basename(script)}
+	 EOH
 end
 
 ################################################################################
 # Install provision upstart job
 ################################################################################
 
-template '/etc/init/build-ami' do
-	source 	"provision.conf.erb"
+template '/etc/init/update-aws-tag.conf' do
+	source 	"update-aws-tag.conf.erb"
 	mode 	0755
 	owner 	"root"
 	group 	"root"
+	varaiables({
+		:script => script
+	})
 end
