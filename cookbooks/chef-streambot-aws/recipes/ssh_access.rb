9f0d1ad10c7223719b28b1e006c0ef3d2b3ca637
Martin Biermann <info@martinbiermann.com>
2014-01-31 17:09:14 +0100

    Adds SSH keypair installation

--- cookbooks/chef-streambot-aws/recipes/ssh_access.rb
+++ cookbooks/chef-streambot-aws/recipes/ssh_access.rb
@@ -1,4 +1,25 @@
 node.ssh_access.ssh.authorized_keys.each do |key|
-	file = "#{node.ssh_access.ssh.user_home}/.ssh/authorized_keys"
+	file = "#{node.ssh_access.ssh.user.home}/.ssh/authorized_keys"
     execute "if ! grep -q '#{key}' #{file} ; then echo '#{key}' >> #{file}; fi;"
+end
+
+
+unless node.ssh_access.ssh.user.private_key.nil?
+	bash "install_private_key_for_user_#{node.ssh_access.ssh.user.name}" do
+		user 	node.ssh_access.ssh.user.name
+		cwd		node.ssh_access.ssh.user.home
+		code 	<<-EOH
+			echo "#{node.ssh_access.ssh.user.private_key}" > .ssh/id_rsa
+		EOH
+	end
+end
+
+unless node.ssh_access.ssh.user.public_key.nil?
+	bash "install_public_key_for_user_#{node.ssh_access.ssh.user.name}" do
+		user 	node.ssh_access.ssh.user.name
+		cwd		node.ssh_access.ssh.user.home
+		code 	<<-EOH
+			echo "#{node.ssh_access.ssh.user.public_key}" > .ssh/id_rsa.pub
+		EOH
+	end
 end
