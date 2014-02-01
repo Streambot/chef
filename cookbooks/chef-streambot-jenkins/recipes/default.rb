b31f32ab0cc243627bd8af501f31b9bb0dcb2e4f
Martin Biermann <info@martinbiermann.com>
2014-02-01 17:27:16 +0100

    Missing adjustments in default recipe for Jenkins to adopt new Jenkins cookbook version 2.0.2

--- cookbooks/chef-streambot-jenkins/recipes/default.rb
+++ cookbooks/chef-streambot-jenkins/recipes/default.rb
@@ -1,4 +1,5 @@
-include_recipe "jenkins::server"
+include_recipe "jenkins::java"
+include_recipe "jenkins::master"
 include_recipe "chef-streambot-jenkins::s3"
 include_recipe "chef-streambot-jenkins::thin_backup"
 include_recipe "chef-streambot-jenkins::backup_s3_upload"
