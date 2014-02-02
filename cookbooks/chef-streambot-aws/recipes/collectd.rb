84315572bf92262236d37046412f0ba84e99a813
Martin Biermann <info@martinbiermann.com>
2014-02-02 12:09:34 +0100

    Adds missing recourses for collectd provisioning

---
+++ cookbooks/chef-streambot-aws/recipes/collectd.rb
@@ -0,0 +1,34 @@
+# Get the add-apt-repository command
+execute "install add-apt-repository" do
+  command "sudo apt-get install python-software-properties -y"
+end
+
+# Add ppa to make version 1.7+ of Subversion available
+execute "add-apt-repository" do
+  command "sudo add-apt-repository ppa:kmscherer/collectd"
+end
+
+# apt-get update to bring in the ppa contents
+execute "apt-get update" do
+  command "sudo apt-get update"
+end
+
+# apt-get install subversion
+package "collectd" do
+  action :install
+end
+
+# Install Graphite configuration for Collectd from template
+
+graphite_conf_file = "/etc/collectd/graphite.conf"
+
+template graphite_conf_file do
+	source "graphite.collectd.conf.erb"
+	chmod 0644
+	owner "root"
+	group "root"
+	variables(node[:collectd])
+end
+
+# Add an include on the Graphite configuration for collectd to main collectd configuration
+bash "echo \"Include \\\"#{graphite_conf_file}\\\"\" >> /etc/collectd/collectd.conf"
