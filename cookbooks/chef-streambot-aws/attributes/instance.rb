c8c1a1596a71c700b7503ac24dc8282472a0b376
Martin Biermann <info@martinbiermann.com>
2014-02-02 18:23:18 +0100

    Fixes startup script execution order around AWS instance recipe

--- cookbooks/chef-streambot-aws/attributes/instance.rb
+++ cookbooks/chef-streambot-aws/attributes/instance.rb
@@ -6,8 +6,9 @@ default[:collect][:graphite_host] = "localhost"
 
 # Define startup scripts to be installed
 # Order is important here!
-default[:aws_tags_script] = "/etc/update-aws-tags.sh"
-default[:startup_scripts]["1"] = default[:aws_tags_script]
-
 default[:hostname_script] = "/etc/update-hostname.sh"
-default[:startup_scripts]["2"] = default[:hostname_script]
+default[:startup_scripts]["1"] = default[:hostname_script]
+
+default[:aws_tags_script] = "/etc/update-aws-tags.sh"
+default[:startup_scripts]["2"] = default[:aws_tags_script]
+
