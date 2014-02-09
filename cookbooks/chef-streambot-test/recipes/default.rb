daf6bdaf3976bd691265a12a38fe9be874a89d3e
Martin Biermann <info@martinbiermann.com>
2014-02-10 00:26:51 +0100

    Fixes test deployment

--- cookbooks/chef-streambot-test/recipes/default.rb
+++ cookbooks/chef-streambot-test/recipes/default.rb
@@ -1,3 +1,14 @@
+include_recipe "apt"
+include_recipe "statsd"
+include_recipe "golang"
+include_recipe "chef-golang"
+include_recipe "golang::packages"
+
+# Verify, that StatsD is running on the machine
+service "statsd" do
+	action :restart
+end
+
 ##############################################################################
 # Install test's user
 ##############################################################################
