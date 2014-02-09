27c48b5e7d89931349e92fe5748db0ee242897b6
Martin Biermann <info@martinbiermann.com>
2014-02-09 17:41:11 +0100

    Fixes missing API home folder by creating it prior to any access happening by the recipes

--- cookbooks/chef-streambot-api/recipes/default.rb
+++ cookbooks/chef-streambot-api/recipes/default.rb
@@ -4,6 +4,15 @@ include_recipe "statsd"
 include_recipe "golang"
 include_recipe "chef-golang"
 include_recipe "golang::packages"
+
+directory node[:streambot_api][:home] do
+  owner "root"
+  group "root"
+  mode  "0644"
+  action :create
+  recursive true
+end
+
 include_recipe "chef-streambot-api::gom_packages"
 
 # Verify, that StatsD is running on the machine
