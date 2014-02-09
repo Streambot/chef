03b45fc000fdb30e46f60a47107a7bce15c56335
Martin Biermann <info@martinbiermann.com>
2014-02-09 19:14:05 +0100

    Fixes gom installations in API provisioning

--- cookbooks/chef-streambot-api/recipes/gom_packages.rb
+++ cookbooks/chef-streambot-api/recipes/gom_packages.rb
@@ -1,8 +1,13 @@
-template "#{node[:streambot_api][:home]}/Gomfile" do
+template "#{node[:streambot_api][:src]}/Gomfile" do
 	source "Gomfile.erb"
 	owner  "root"
 	mode   "744"
 	variables({
 		:deps => node[:gom_packages]
 	})
+end
+
+bash "install_gom_dependencies" do
+	cwd node[:streambot_api][:src]
+	code "gom install"
 end
